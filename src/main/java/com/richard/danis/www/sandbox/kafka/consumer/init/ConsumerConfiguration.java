2ea02510c028db34543d2597bc493eb8ac870d17
Richard Danis <Richard_Danis@epam.com>
2020-04-03 11:37:33 +0200

    add consumer

---
+++ src/main/java/com/richard/danis/www/sandbox/kafka/consumer/init/ConsumerConfiguration.java
@@ -0,0 +1,40 @@
+package com.richard.danis.www.sandbox.kafka.consumer.init;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+
+import java.util.Map;
+
+@Configuration
+public class ConsumerConfiguration {
+
+    @Value(value = "${kafka.bootstrapAddress}")
+    private String bootstrapAddress;
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, String> fooKafkaListenerContainerFactory() {
+        return kafkaListenerContainerFactory("test");
+    }
+
+    private ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory(String groupId) {
+        ConcurrentKafkaListenerContainerFactory<String, String> factory =
+                new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory(groupId));
+        return factory;
+    }
+
+    private ConsumerFactory<String, String> consumerFactory(String groupId) {
+        Map<String, Object> props = Map.of(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress,
+                                           ConsumerConfig.GROUP_ID_CONFIG, groupId,
+                                           ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class,
+                                           ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        return new DefaultKafkaConsumerFactory<>(props);
+    }
+
+}
---
+++ src/main/java/com/richard/danis/www/sandbox/kafka/consumer/service/ConsumerService.java
@@ -0,0 +1,17 @@
+package com.richard.danis.www.sandbox.kafka.consumer.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ConsumerService {
+
+    private final Logger LOGGER = LoggerFactory.getLogger(ConsumerService.class);
+
+    @KafkaListener(topics = "testTopic", groupId = "test")
+    public void listen(String message) {
+        LOGGER.info("Received Message in group test: {}", message);
+    }
+}
