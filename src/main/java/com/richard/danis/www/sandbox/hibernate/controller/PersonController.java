ccd46d8d13f10250f744f677492a44400c7a5bc3
Richard Danis <Richard_Danis@epam.com>
2020-04-06 13:07:56 +0200

    add api endpoint

--- src/main/java/com/richard/danis/www/sandbox/hibernate/controller/PersonController.java
+++ src/main/java/com/richard/danis/www/sandbox/hibernate/controller/PersonController.java
@@ -2,13 +2,22 @@ package com.richard.danis.www.sandbox.hibernate.controller;
 
 import com.richard.danis.www.sandbox.hibernate.model.Person;
 import com.richard.danis.www.sandbox.hibernate.service.PersonService;
+import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.PostConstruct;
+import javax.persistence.PostRemove;
+import java.util.NoSuchElementException;
 
-@RestController("/person")
+@RestController
+@RequestMapping("/person")
 public class PersonController {
 
     private final Logger LOGGER = LoggerFactory.getLogger(PersonController.class);
@@ -18,10 +27,25 @@ public class PersonController {
         this.personService = personService;
     }
 
+    @GetMapping("/{id}")
+    @ApiOperation(value = "Find person by id", response = Person.class, responseContainer = "object")
+    public ResponseEntity<Person> findById(@PathVariable long id) {
+        try {
+            Person person = personService.findById(id);
+            return new ResponseEntity<>(person, HttpStatus.OK);
+        } catch (NoSuchElementException e) {
+            LOGGER.warn("Person element not found in database: {}", id);
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } catch (Exception e) {
+            LOGGER.error("Unrecognized error for find person by id: {} , exception: {}", id, e);
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
     @PostConstruct
     public void init() {
         Person person = new Person();
-        person.setName("test");
+        person.setName("Fruzsi");
         Person savedPerson = personService.save(person);
         LOGGER.info("Person saved into database: {}", savedPerson);
     }
